07570eda814dac62cdca49015cfdd63bf4c81a57
QuentinHt <huet-quentin@hotmail.fr>
2021-04-09 10:08:16 +0200

    Create data à la validation d'une question

--- www/scripts/admin/app.js
+++ www/scripts/admin/app.js
@@ -1,11 +1,35 @@
 // Globals
 
-// Login variable
-
 let questionType = document.formQuizz.typeQuestion;
-let form = document.querySelector('#createQuizz form');
+let createQuizz = document.querySelector('#createQuizz')
+let form = createQuizz.querySelector('form');
 let containerQuestions = form.querySelector('.questions');
 let allQuestions = containerQuestions.querySelectorAll('div');
+
+// Variables globales
+let numQuestion = createQuizz.querySelector('h2 span');
+let dataQuizz = [];
+
+// Container question
+
+let containerText = containerQuestions.querySelector('#text');
+let containerPetitBac = containerQuestions.querySelector('#petitBac')
+
+// Button next
+let nextText = containerText.querySelector('.nextText');
+let nextPetitBac = containerQuestions.querySelector('.nextPetitBac');
+
+// Questions next
+let questionText = containerText.querySelector('#textQuestion');
+let reponseText = containerText.querySelector('#textReponse');
+
+// Questions petit bac
+let letterPetitBac = containerPetitBac.querySelector('#petitBacLetter');
+let FirstPetitBac = containerPetitBac.querySelector('#petitBac1');
+let SecondPetitBac = containerPetitBac.querySelector('#petitBac2');
+let ThirdPetitBac = containerPetitBac.querySelector('#petitBac3');
+let FourthPetitBac = containerPetitBac.querySelector('#petitBac4');
+let FifthPetitBac = containerPetitBac.querySelector('#petitBac5');
 
 // Globals functions
 
@@ -24,5 +48,28 @@ for (var i = 0; i < questionType.length; i++) {
         containerQuestions.querySelector(`div:nth-of-type(${this.value})`).classList.remove('hidden');
     });
 }
+
+nextText.addEventListener('click', function(e){
+    e.preventDefault();
+    dataQuizz.push({
+        'num': numQuestion.textContent,
+        'question': questionText.value,
+        'reponse': reponseText.value
+    })
+    console.log(dataQuizz)
+});
+nextPetitBac.addEventListener('click', function(e){
+    e.preventDefault();
+    dataQuizz.push({
+        'num': numQuestion.textContent,
+        'letter': letterPetitBac.value,
+        'theme1': FirstPetitBac.value,
+        'theme2': SecondPetitBac.value,
+        'theme3': ThirdPetitBac.value,
+        'theme4': FourthPetitBac.value,
+        'theme5': FifthPetitBac.value
+    })
+    console.log(dataQuizz)
+});
 
 //changeFormLog.addEventListener('click', changeFormLogin);
