b3ed43124d0a51ed553ed4214086542f5c9d99a8
QuentinHt <huet-quentin@hotmail.fr>
2021-04-16 00:20:49 +0200

    Start quizz fonctionnel

--- www/scripts/quizz/app.js
+++ www/scripts/quizz/app.js
@@ -4,15 +4,57 @@
 
 let sectionStart = document.getElementById('accueilQuizz');
 let sectionPlay = document.getElementById('gameQuizz');
+let allSectionQuestion = sectionPlay.querySelectorAll('div');
 
 let buttonStart = sectionStart.querySelector('button');
-// Globals functions
 
-// Login functions
+// Question text
+let containerText = sectionPlay.querySelector('.text');
+let h2Text = containerText.querySelector('h2');
+let inputText = containerText.querySelector('input');
+
+// Question Petit bac
+let containerPetitBac = sectionPlay.querySelector('.petitBac');
+let letter = containerPetitBac.querySelector('h2');
+let themeLabel = containerPetitBac.querySelectorAll('label');
+let themeInput = containerPetitBac.querySelectorAll('input');
+
+const quizz = JSON.parse(dataQuizz);
+// Globals functions
 
 let startQuizz = () => {
     sectionStart.classList.add('hidden');
     sectionPlay.classList.remove('hidden');
+
+    if(quizz.questions[0].type == 1){
+        textQuestion(quizz.questions[0]);
+    }
+    else if(quizz.questions[0].type == 2){
+        petitBacQuestion(quizz.questions[0]);
+    }
+};
+
+let nextQuestion = () => {
+    // for(let i = 0; i < quizz.questions.length; i ++){
+    //     console.log(quizz.questions[i]);
+    // }
+};
+
+let hiddenContainers = () => {
+    allSectionQuestion.forEach(question => {
+        question.classList.add('hidden');
+    })
+}
+
+// Function Questions
+let textQuestion = () => {
+    hiddenContainers();
+    containerText.classList.remove('hidden');
+}
+
+let petitBacQuestion = () => {
+    hiddenContainers();
+    containerPetitBac.classList.remove('hidden');
 }
 
 // Global Events
